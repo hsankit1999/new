f62f22b82fb10e70801c812b86571949d042bb59
Ankit Sharma <hsankit1999@gmail.com>
2017-12-01 23:30:56 +0530

    Initial

---
+++ server.js
@@ -0,0 +1,16 @@
+var express=require("express");
+var path=require('path');
+
+var app=express();
+var port=process.env.PORT || 3000;
+var publicPath=path.join(__dirname,"public");
+app.use(express.static(publicPath));
+/*    res.send("Hello  ...");
+});*/
+app.get('/',function(req,res){
+    res.render("index");
+});
+
+app.listen(5002,function(){
+    console.log('Server started');
+});
