59f94d6302f7ccf50dadd97f279befb1c48b87db
Ankit <hsankit1999@gmail.com>
2017-12-02 21:59:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 var express=require("express");
 var path=require('path');
-
+var os=require('os');
 var app=express();
 var port=process.env.PORT || 3000;
 var publicPath=path.join(__dirname,"public");
-app.use(express.static(publicPath));
+//app.use(express.static(publicPath));
 /*    res.send("Hello  ...");
 });*/
 app.get('/',function(req,res){
-    res.render("index");
+    res.send(os.networkInterfaces());
 });
+
+
 
 app.listen(port,function(){
     console.log('Server started');
